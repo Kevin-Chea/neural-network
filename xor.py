26db24b2e69b01cac0b87110c367fd9cfd545f55
Kevin Chea <kcl.chea@gmail.com>
2025-01-01 12:00:01 +0100

    [ADD][XOR] Simple XOR

---
+++ xor.py
@@ -0,0 +1,27 @@
+from neural_network import Neural_Network
+
+# Create neural network
+nn = Neural_Network()
+# A 3 neurons layer
+nn.add_layer(3, 2)
+# 1 output
+nn.add_layer(1, 3)
+
+inputs = [
+    [0, 0],
+    [1, 0],
+    [0, 1],
+    [1, 1],
+]
+
+outputs = [
+    [0],
+    [1],
+    [1],
+    [0],
+]
+
+nn.train(inputs, outputs, 50000, 0.1)
+for input in inputs:
+    print(nn.forward(input))
+print(nn)
