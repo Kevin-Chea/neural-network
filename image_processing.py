a97c4ab1d940a0a63868d45ea08905b5edc6e35f
Kevin Chea <kcl.chea@gmail.com>
2025-02-01 23:14:09 +0100

    [FIX][IMG_PROC] Fix load_batch_from_pickle, now return expected number of images per batch

--- image_processing.py
+++ image_processing.py
@@ -13,6 +13,10 @@ def pre_process_image(path):
     pixels = [pixel / 255.0 for pixel in img_resized.getdata()]
     return pixels
 
+
+def inverse_colors(image_data):
+    return [1.0 - pixel for pixel in image_data]
+
 def files_of_folder(path: str):
     """
     Returns all the files names in a folder.
@@ -53,7 +57,8 @@ def pre_process_and_save_images_of_n_to_pickle(n: int, output_file: str, limit: 
     all_data = []
     for image_file in first_images_of_n(n, limit):
         pixels = pre_process_image(image_file)
-        all_data.append(pixels)
+        inverted_pixels = inverse_colors(pixels)
+        all_data.append(inverted_pixels)
     with open(output_file, 'wb') as f:
         pickle.dump(all_data, f)
 
@@ -62,7 +67,7 @@ def pre_process_all_numbers(limit=1000):
     for i in range(0, 10):
         pre_process_and_save_images_of_n_to_pickle(i, "train_" + str(i), limit)
 
-def load_batch_from_pickle(file_path: str, batch_size: int, batch_index: int):
+def load_batch_from_pickle(file_path: str, images_per_batch: int, batch_index: int):
     """
     Load batch data.\n
     Params:\n
@@ -72,19 +77,40 @@ def load_batch_from_pickle(file_path: str, batch_size: int, batch_index: int):
     """
     with open(file_path, "rb") as f:
         data = pickle.load(f)
-    start = batch_index * batch_size
-    end = start + batch_size
+    start = batch_index * images_per_batch
+    end = start + images_per_batch
     return data[start:end]
 
 def load_batch_for_all_numbers(images_per_batch: int=100):
     data = []
     expected_output = []
+    
+    label_counts = {i: 0 for i in range(10)}
     for i in range(10):
         # 28 * 28 * 100 : image size * nb of images in a batch
-        data.extend(load_batch_from_pickle("train_" + str(i), 28 * 28 * images_per_batch, 0))
+        data.extend(load_batch_from_pickle("train_" + str(i), images_per_batch, 0))
         expected_output.extend([i] * images_per_batch)
+        label_counts[i] += images_per_batch
     # Shuffle
     combined = list(zip(data, expected_output))
     random.shuffle(combined)
     data, expected_output = zip(*combined)
     return list(data), list(expected_output)
+
+
+def show_image_from_batch(image_data):
+    """
+    Show an image from extracted data (batch)
+    
+    Params:
+    - image_data (list): list of 784 values (28x28) normalized between 0 and 1.
+    """
+    # Convertir les valeurs en niveaux de gris (0-255)
+    pixel_values = [int(p * 255) for p in image_data]
+
+    # Créer une image en niveaux de gris (L) avec PIL
+    img = Image.new('L', (28, 28))
+    img.putdata(pixel_values)
+
+    # Afficher l'image
+    img.show()
