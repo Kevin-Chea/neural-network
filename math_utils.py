7a78d12c0ce8ea99f25fb9dd647af7ccfe7fc5db
Kevin Chea <kcl.chea@gmail.com>
2025-02-03 20:45:36 +0100

    [ADD][MATH] Softmax and categorical loss entropy functions

--- math_utils.py
+++ math_utils.py
@@ -9,3 +9,16 @@ def sigmoid_derivative(x):
 
 def relu(x):
     return max(0, x)
+
+def softmax(inputs):
+    """
+    Compute softmax of a vector inputs.
+    """
+    exp_values = [math.exp(x) for x in inputs]
+    total = sum(exp_values)
+    return [exp_value / total for exp_value in exp_values]
+
+def categorical_loss_entropy(expected_one_hot, pred_one_hot):
+    epsilon = math.exp(-12) # Avoid log(0) issues
+    pred_one_hot = [max(min(p, 1 - epsilon), epsilon) for p in pred_one_hot] # Clip values between epsilon and 1 - epsilon
+    return -sum(e * math.log(p) for e,p in zip(expected_one_hot, pred_one_hot))
