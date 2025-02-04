8658f6fef8ae631e7d7cad00876f61780164cd87
Kevin Chea <kcl.chea@gmail.com>
2025-02-04 20:32:02 +0100

    [ADD][OCR] Change forward in layer in case of softmax

--- layer.py
+++ layer.py
@@ -1,12 +1,20 @@
 from neuron import Neuron
-from math_utils import sigmoid
+from math_utils import sigmoid, sigmoid_derivative, softmax
 
 class Layer:
-    def __init__(self, nb_neurons, nb_inputs, activation_function=sigmoid):
-        self.neurons = [Neuron(nb_inputs=nb_inputs, activation_function=activation_function) for _ in range(nb_neurons)]
+    def __init__(self, nb_neurons, nb_inputs, activation_function=sigmoid, derivative_function=sigmoid_derivative):
+        self.neurons = [Neuron(nb_inputs=nb_inputs, activation_function=activation_function, derivative_activation_function=derivative_function) for _ in range(nb_neurons)]
         
     def forward(self, inputs):
-        self.outputs = [neuron.activate(inputs) for neuron in self.neurons]
+        if self.neurons[0].activation_function == softmax:
+            all_z = [sum(w * x for w,x in zip(neuron.weights, inputs)) + neuron.bias for neuron in self.neurons]
+            self.outputs = softmax(all_z)
+            for (i, neuron) in enumerate(self.neurons):
+                neuron.inputs = inputs
+                neuron.z = all_z[i]
+                neuron.output = self.outputs[i]
+        else:
+            self.outputs = [neuron.activate(inputs) for neuron in self.neurons]
         return self.outputs
     
     def backward(self, output_errors):
